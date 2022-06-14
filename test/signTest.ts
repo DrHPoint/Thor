9565f4ca7c4ee53adced17a2fbff2d5dc584abb0
DrHPoint <hpedlk1007@gmail.com>
2022-06-14 20:13:56 +0700

    Add sign part

---
+++ test/signTest.ts
@@ -0,0 +1,72 @@
+import { EIP712Domain } from 'eth-typed-data'
+import hre, { deployments, ethers, network } from "hardhat";
+
+describe("Report", function () {
+
+    const domain = new EIP712Domain({
+        name: 'Kreolarge',               
+        version: '2.4.97',                     
+        chainId: 10798,                       
+        verifyingContract: '0xbad0feed',  
+        salt: 'holyDolly123'              
+    })
+
+    const Person = domain.createType('Person', {
+        name: 'string',
+        wallet: 'address'
+    })
+
+    const Report = domain.createType('Report', {
+        from: 'Person',
+        to: 'Person',
+        content: 'string',
+        date: 'uint256'
+    })
+
+
+
+    describe("EIP712", () => {
+
+        let dora = new Person({
+            name: 'Daria',
+            wallet: '0x74154cb67d4f13fb2b7f28b482dce9002339b1fb'
+        })
+
+        let innocent = new Person({
+            name: 'Innocent',
+            wallet: '0xf2d4b8cf0b8edf2530a89ae2cdef5ee2b7837302'
+        })
+
+        let excuse = new Report({
+            from: innocent,
+            to: dora,
+            content: 'Hello! How are you?'
+        })
+
+        let quilty = new Report({
+            from: {
+                name: 'Daria',
+                wallet: '0x74154cb67d4f13fb2b7f28b482dce9002339b1fb'
+            },
+            to: {
+                name: 'Innocent',
+                wallet: '0xf2d4b8cf0b8edf2530a89ae2cdef5ee2b7837302'
+            },
+            content: 'Sorry! I ate your pudding'
+        })
+
+        it("Sign", async () => {
+
+        console.log(await excuse.toSignatureRequest());
+
+        await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [metisWhaleAddress],
+        });
+        const metisWhale = ethers.provider.getSigner(metisWhaleAddress);
+
+        });
+
+    });
+
+});
